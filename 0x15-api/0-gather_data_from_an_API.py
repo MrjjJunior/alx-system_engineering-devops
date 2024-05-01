6e0b94d3d17c995afc3cc54fe8136a60bac912ef
Tshepiso <tlhongtshepiso2@gmail.com>
2024-05-01 12:04:35 +0200

    gather data

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,26 +1,37 @@
 #!/usr/bin/python3
-"""
-Returns to-do list information for a given employee ID.
-
-This script takes an employee ID as a command-line argument and fetches
-the corresponding user information and to-do list from the JSONPlaceholder API.
-It then prints the tasks completed by the employee"""
-
+"""Gathers data from an API."""
 import requests
 import sys
 
-
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
+    if len(sys.argv) != 2:
+        print("Usage: {} <employee_id>".format(sys.argv[0]))
+        sys.exit(1)
 
     employee_id = sys.argv[1]
-    user = requests.get(url + "users/{}".format(employee_id)).json()
+    url_user = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
+    url_todos = "https://jsonplaceholder.typicode.com/todos?userId={}".format(employee_id)
 
-    params = {"userId": employee_id}
-    todos = requests.get(url + "todos", params).json()
+    response_user = requests.get(url_user)
+    response_todos = requests.get(url_todos)
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(complete)) for complete in completed]
+    if response_user.status_code != 200:
+        print("Error: User data not found")
+        sys.exit(1)
+
+    if response_todos.status_code != 200:
+        print("Error: Todo data not found")
+        sys.exit(1)
+
+    user_data = response_user.json()
+    todos_data = response_todos.json()
+
+    total_tasks = len(todos_data)
+    completed_tasks = [task for task in todos_data if task['completed']]
+    total_completed_tasks = len(completed_tasks)
+    employee_name = user_data['name']
+
+    print("Employee {} is done with tasks({}/{}):".format(employee_name, total_completed_tasks, total_tasks))
+    for task in completed_tasks:
+        print("\t {}".format(task['title']))
 
