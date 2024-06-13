13c2a7864836aa4fe0a313fa037abcff5eb2219c
Tshepiso <tlhongtshepiso2@gmail.com>
2024-06-13 11:42:26 +0200

    Reddir API

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,8 +8,8 @@ import requests
 def number_of_subscribers(subreddit):
 #    url = f"https://www.reddit.com/r/{subreddit}/about.json".format(subreddit)
 #    headers = {'User-Agent': 'Mozilla/5.0'}
-url = f"https://www.reddit.com/r/{subreddit}/about.json"
- headers = {"User-Agent": "python:subreddit.subscriber.count:v1.0.0 (by /u/Tshepiso)"}
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "python:subreddit.subscriber.count:v1.0.0 (by /u/Tshepiso)"}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
 
