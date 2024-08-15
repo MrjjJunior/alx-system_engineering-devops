871f26ec5cf08e84e3d4258e0e3f339f70ff9ba3
Tshepiso <tlhongtshepiso2@gmail.com>
2024-08-15 15:21:14 +0200

    revert changes

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,8 +9,7 @@ import requests
 def number_of_subscribers(subreddit):
     ''' counts number of subs '''
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    #headers = {"User-Agent": "Mozilla/5.0"}
-    headers = {"User-Agent": "MyCustomUserAgent/1.0"}
+    headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
